84cc6cb5ff72cf61d828bd88e74aa8db1dfda666
Niels <niels.sw@gmail.com>
2024-05-01 00:09:16 +0200

    improved layout killpage

--- app/src/pages/user/KillPage.vue
+++ app/src/pages/user/KillPage.vue
@@ -1,47 +1,47 @@
 <template>
-  <q-page class="row items-evenly justify-evenly">
+  <q-page class="column justify-evenly items-evenly">
+    <div class="col-shrink row justify-center items-center text-overline">
+      Dags att mörda!
+    </div>
     <div
-      class="col-10 column items-center justify-center"
+      class="col-shrink row justify-center items-center "
     >
-      <div class="col-shrink text-overline">
-        Dags att mörda!
-      </div>
-      <div class="col-shrink">
-        <div class="q-pa-md" style="max-width: 300px">
-          <q-input filled v-model="date">
-            <template v-slot:prepend>
-              <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                  <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
-                    <div class="row items-center justify-end">
-                      <q-btn v-close-popup label="Close" color="primary" flat />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
+      <div class="q-pa-md" style="max-width: 300px">
+        <q-input filled v-model="date">
+          <template v-slot:prepend>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
 
-            <template v-slot:append>
-              <q-icon name="access_time" class="cursor-pointer">
-                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                  <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
-                    <div class="row items-center justify-end">
-                      <q-btn v-close-popup label="Close" color="primary" flat />
-                    </div>
-                  </q-time>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
-        </div>
-      </div>
-      <div class="col-shrink text-h1">
-        {{ killCount }}
+          <template v-slot:append>
+            <q-icon name="access_time" class="cursor-pointer">
+              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-time>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
       </div>
     </div>
-    <q-page-sticky position="bottom-right" :offset="[50,50]">
+    <div class="col row justify-center items-center text-h1">
+      {{ killCount }}
+    </div>
+    <div
+      class="col-shrink row justify-evenly items-center q-my-md"
+    >
       <div
-        class="col row justify-between items-center"
+        class="col-4 row justify-center"
       >
         <q-btn
           rounded
@@ -49,34 +49,54 @@
           label="Avbryt"
           color="negative"
           to="/user"
-          class="q-ma-lg q-pa-lg"
+          class="q-pa-xl"
         ></q-btn>
+      </div>
+      <div
+        class="col-4 row justify-center"
+      >
         <q-btn
           rounded
           size="md"
           label="Färdig"
           color="positive"
           @click="saveKillCount"
-          class="q-ma-lg q-pa-lg"
-        ></q-btn>
-        <q-btn
-          round
-          size="lg"
-          icon="remove"
-          color="primary"
-          @click="killCount=Math.max(0, killCount-1)"
-          class="q-ma-lg q-pa-xl"
-        ></q-btn>
-        <q-btn
-          round
-          size="xl"
-          icon="add"
-          color="primary"
-          @click="killCount++"
-          class="q-ma-lg q-pa-xl"
+          class="q-pa-xl"
         ></q-btn>
       </div>
-    </q-page-sticky>
+    </div>
+    <div
+      class="col-shrink row justify-evenly items-center q-my-md"
+    >
+      <div
+        class="col-4 row justify-center"
+      >
+        <div>
+          <q-btn
+            round
+            size="lg"
+            icon="remove"
+            color="primary"
+            @click="killCount=Math.max(0, killCount-1)"
+            class="q-pa-xl"
+          ></q-btn>
+        </div>
+      </div>
+      <div
+        class="col-4 row justify-center"
+      >
+        <div>
+          <q-btn
+            round
+            size="xl"
+            icon="add"
+            color="primary"
+            @click="killCount++"
+            class="q-pa-xl"
+          ></q-btn>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
